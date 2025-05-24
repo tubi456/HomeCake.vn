c0f80958ce1dd520b441a567b0940afd3f978b97
thuyetnguyen254 <thuyet000254@gmail.com>
2025-05-24 10:21:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,4 +9,16 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     }
   });
-  
+  //Banner
+  document.addEventListener("DOMContentLoaded", function () {
+  let index = 0;
+  const slides = document.querySelectorAll(".slide");
+
+  function showNextSlide() {
+    slides[index].classList.remove("active");
+    index = (index + 1) % slides.length;
+    slides[index].classList.add("active");
+  }
+
+  setInterval(showNextSlide, 3000); // 3 giây
+});
